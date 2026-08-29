f9a4f8479f640b9d28f630ddab41f449839facd2
naveedkhan-png <p260036@pwr.nu.edu.pk>
2026-08-29 02:49:30 -0700

    Add files via upload

---
+++ p260036_task2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+printf("Printing academic details:\n");
+int batch = 2026;
+printf("=======================================================\n\tFAST-NUCES STUDENT PROFILE CARD\n");
+printf("=======================================================\n");
+printf("Batch:\t\t\t%d\n",batch);
+char section = 'A';
+printf("Section:\t\t%c\n",section);
+float test_score = 82.75;
+printf("Test Score:\t\t%.2f\n",test_score);
+double semester_tuition_fee = 148500.50;
+printf("Semester Fee:\tPKR %.2f\n",semester_tuition_fee);
+printf("Motto:\t\t\t\"Small daily steps lead to big success\"\n");
+printf("=======================================================");
+return 0;
+}
+
